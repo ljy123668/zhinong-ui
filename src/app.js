33ddc7b8794b8207991df2e663126d0d1b8c0534
赵伟棋 <651507008@qq.com>
2020-11-17 21:25:54 +0800

    使用chai-spies 监听click事件

--- src/app.js
+++ src/app.js
@@ -14,3 +14,106 @@ new Vue({
     loading3: false,
   }
 })
+
+import chai from 'chai'
+import spies from 'chai-spies'
+chai.use(spies)
+const expect = chai.expect
+
+//单元测试
+
+{
+
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: "setting"
+    }
+  })
+  vm.$mount()
+  let useElement = vm.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-setting')
+  vm.$el.remove()
+  vm.$destroy()
+}
+
+{
+
+
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: "setting",
+      loading: true
+    }
+  })
+  vm.$mount()
+  let useElement = vm.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#i-loading')
+  vm.$el.remove()
+  vm.$destroy()
+}
+
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: "setting"
+    }
+  })
+  vm.$mount(div)
+  let svg = vm.$el.querySelector('svg')
+  let {
+    order
+  } = window.getComputedStyle(svg)
+
+  expect(order).to.eq("1")
+  vm.$el.remove()
+  vm.$destroy()
+}
+
+
+{
+  const div = document.createElement('div')
+  document.body.appendChild(div)
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: "setting",
+      iconPosition: 'right'
+    }
+  })
+  vm.$mount(div)
+  let svg = vm.$el.querySelector('svg')
+  let {
+    order
+  } = window.getComputedStyle(svg)
+
+  expect(order).to.eq("2")
+  vm.$el.remove()
+  vm.$destroy()
+}
+
+{
+  //mock
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: "setting",
+      iconPosition: 'right'
+    }
+  })
+  vm.$mount()
+  let spy = chai.spy(() => {
+
+  })
+  vm.$on('click', spy)
+  //希望函数被执行
+  let button = vm.$el
+  button.click()
+  expect(spy).to.have.been.called()
+}
