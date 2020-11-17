132bf5706246cbafde0dfa41d4232978ce0549a2
赵伟棋 <651507008@qq.com>
2020-11-17 14:17:05 +0800

    验证iconPosition

--- src/button.vue
+++ src/button.vue
@@ -20,7 +20,17 @@
 </template>
 <script>
 export default {
-  props: ["icon", "iconPosition"],
+  props: {
+    icon: {},
+    iconPosition: {
+      type: String,
+      default: "left",
+      validator(value) {
+        console.log(value);
+        return value === "left" || value === "right";
+      },
+    },
+  },
 };
 </script>
 <style lang="scss">
